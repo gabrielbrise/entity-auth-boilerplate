90bb1f52df00f5345b4eff7a2a60cdb5a6b17de0
gabrielbrise <gabriel_ns@live.com>
2022-11-19 00:43:32 -0300

    Add project files.

---
+++ AuthBoilerplate/Utils/Database.cs
@@ -0,0 +1,15 @@
+﻿using AuthBP.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthBoilerplate.Utils
+{
+    public static class DatabaseUtils
+    {
+        public static void RunMigrations(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<AuthBoilerplateDbContext>();
+            db.Database.Migrate();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+### Package Manager Console
+
+#### Migrations
+##### Creating Migrations
+```Add-Migration nameofthemigration```
+
+##### Update Database
+```Update-Database```
